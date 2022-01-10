0df2337a4dce92f15da6bf34b1b98b3d73f55dd8
James Wu <james@miracle.co.nz>
2022-01-10 15:26:51 +1300

    add comments

--- vidly/app.js
+++ vidly/app.js
@@ -16,7 +16,9 @@ app.get('/api/genres/', (req, res) => {
     res.send(genresDatabase);
 });
 
-const port = 3000 || process.env.PORT;
+// beware that the order of this operation
+// if process.env.PORT is defined, it will use PORT first
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
