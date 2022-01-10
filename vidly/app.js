afad8f8dbc83023ecdf44a0e6a398fe4c881dcf4
James Wu <james@miracle.co.nz>
2022-01-11 08:17:25 +1300

    add put

--- vidly/app.js
+++ vidly/app.js
@@ -29,6 +29,17 @@ app.post(API_ADDR, (req, res) => {
     res.send(genre);
 });
 
+app.put('/api/genres/:id', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const genre = genresDatabase.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(400).send('The genre with the given ID was not found.');
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
 // beware that the order of this operation
 // if process.env.PORT is defined, it will use PORT first
 const port = process.env.PORT || 3000;
