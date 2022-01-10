ee358ed565c2b00485e355aea2a6e7b6bd6a6074
James Wu <james@miracle.co.nz>
2022-01-10 13:35:28 +1300

    added get command

---
+++ vidly/app.js
@@ -0,0 +1,31 @@
+const Express = require('express');
+const { func } = require('joi');
+const app = Express();
+app.use(Express.json());
+
+const Joi = require('joi');
+
+const genresDatabase = [
+    { id: 1, name: 'Action'     },
+    { id: 2, name: 'SciFi'      },
+    { id: 3, name: 'Drma'       },
+    { id: 4, name: 'Romance'    },
+];
+
+app.get('/api/genres/', (req, res) => {
+    res.send(genresDatabase);
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+function validateGenre(genre)
+{
+    const scheme = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return scheme.validate({name: genre.name});
+}
