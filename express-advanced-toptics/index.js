7f94dddaa32dc38ab4de7968dbc325050881bbca
James Wu <james@miracle.co.nz>
2022-01-14 15:50:20 +1300

    add comments

--- express-advanced-toptics/index.js
+++ express-advanced-toptics/index.js
@@ -5,22 +5,28 @@ const logger = require('./logger');     // my middleware
 const hemlet = require('helmet');       // set http header
 const morgan = require('morgan');       // 3rd party logger
 const config = require('config');       // environment tool
-const debug = require('debug')('debug');
+const debug = require('debug')('debug');        // debug package
 
-const courses = require('./routes/courses');
-const index = require('./routes/index');
+// import routes
+const courses = require('./routes/courses');    // courses route
+const index = require('./routes/index');        // index route
 
+// middlewares
 app.use(hemlet());
 app.use(Express.json());
 app.use(Express.urlencoded({extended: true}));
 app.use(Express.static('public'));
 //app.use(logger.logger);
+
+// using route
 app.use('/api/courses', courses);
 app.use('/', index);
 
+// template engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// testing config package
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
     console.log('Morgan enable...');
@@ -31,5 +37,6 @@ console.log(`APP NAME: ${config.get('name')}`);
 console.log(`MAIL SRV: ${config.get('mail.server')}`);
 console.log(`MAIL PASS: ${config.get('mail.password')}`);
 
+// start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
