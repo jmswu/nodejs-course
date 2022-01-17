c61139f7cc70446bbcfbe69b58909a5b0db90c39
James Wu <james@miracle.co.nz>
2022-01-18 09:23:24 +1300

    added function callback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,13 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from database...');
-}, 2000);
+getUser(1, (user) => {
+    console.log("User: ", user);
+});
 console.log('After');
+
+function getUser(id, callback)
+{
+    setTimeout(() => {
+        console.log('Reading a user from database...');
+        callback( {id: id, gitHubName: 'James'} );
+    }, 1000);
+}
