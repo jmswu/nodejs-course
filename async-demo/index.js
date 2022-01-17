c4d4db276a5b707308c4dedd9908fd49a61bc903
James Wu <james@miracle.co.nz>
2022-01-18 09:33:44 +1300

    added extra callback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,9 @@
 console.log('Before');
 getUser(1, (user) => {
     console.log("User: ", user);
+    getRepositories(user, (repos) =>{
+        console.log('Repos:', repos);
+    })
 });
 console.log('After');
 
@@ -10,4 +13,13 @@ function getUser(id, callback)
         console.log('Reading a user from database...');
         callback( {id: id, gitHubName: 'James'} );
     }, 1000);
+}
+
+function getRepositories(userName, callback)
+{
+    setTimeout(() => {
+        console.log('Reading repositories...', userName);
+        const repos = ['repo1', 'repo2', 'repo3'];
+        callback(repos);
+    }, 2000);
 }
