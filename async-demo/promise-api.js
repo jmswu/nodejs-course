089730945806d5c5655a9f8d896af3dd53f735bb
James Wu <jameswu62@yahoo.co.nz>
2022-01-19 14:45:16 +1300

    play around with Promise.all() and Promise.race() apis

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,2 +1,20 @@
-const p = Promise.resolve({id: 1});
-p.then(resolved => console.log(resolved));
+const p1 = new Promise((resolved) => {
+    setTimeout(() => {
+        console.log('Reading API 1...');
+        resolved({id: 1, name: 'hello', text: 'extra text'});
+    }, 3000)
+});
+
+const p2 = new Promise((resolved) => {
+    setTimeout(() => {
+        console.log('Reading API 2...');
+        resolved({id: 23, name: 'blah'});
+    }, 1000)
+});
+
+
+// Promise.race([p1, p2])
+//     .then(result => console.log(result));
+
+Promise.all([p1, p2])
+    .then(result => console.log(result));
