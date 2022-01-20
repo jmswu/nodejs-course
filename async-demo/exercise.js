d10e6cf186c52597610ab161bffafd058ed834c4
James Wu <james@miracle.co.nz>
2022-01-20 15:35:00 +1300

    added exercise

--- async-demo/exercise.js
+++ async-demo/exercise.js
@@ -1,35 +1,61 @@
 
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
+// getCustomer(1, (customer) => {
+//   console.log('Customer: ', customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log('Top movies: ', movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log('Email sent...')
+//       });
+//     });
+//   }
+// });
+
+notifiCustomer(1);
+
+async function notifiCustomer(id)
+{
+  try{
+    const customer = await getCustomer(1);
+    console.log('customer: ', customer);
+    if (customer.isGold)
+    {
+      const movies = await getTopMovies();
+      console.log('Top Movies: ', movies);
+      await sendEmail(customer.email, movies);
+    }
   }
-});
-
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
+  catch(err)
+  {
+    console.log(err);
+  }
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getCustomer(id) {
+  return new Promise((resolved, reject) => {
+    setTimeout(() => {
+      resolved({ 
+        id: 1, 
+        name: 'Mosh Hamedani', 
+        isGold: true, 
+        email: 'email' 
+      });
+    }, 4000);  
+  });
 }
 
-function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+function getTopMovies() {
+  return new Promise((resolved, reject) => {
+    setTimeout(() => {
+      resolved(['movie1', 'movie2'])
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolved, reject) => {
+    setTimeout(() => {
+      console.log('Email sent...')
+    }, 4000);
+  });
 }
