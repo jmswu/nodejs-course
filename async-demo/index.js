3b85980cdc39b0198cdde1f24f48ecb29dbe523b
James Wu <jameswu62@yahoo.co.nz>
2022-01-20 13:34:22 +1300

    added await change

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,13 +1,33 @@
 console.log('Before');
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubName))
-    .then(repo => getCommit(repo[0]))
-    .then(commit => getCommit(commit))
-    .catch(err => console.log('error: ', err.message));
+// promise base approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubName))
+//     .then(repo => getCommit(repo[0]))
+//     .then(commit => getCommit(commit))
+//     .catch(err => console.log('error: ', err.message));
 
+displayCommit();
 
 console.log('After');
+
+
+// await and async approach
+async function displayCommit()
+{
+    try{
+        const user = await getUser(10);
+        const repo = await getRepositories(user.gitHubName);
+        const commit = await getCommit(repo[0]);
+        console.log('commit', commit);
+    }
+    catch(err)
+    {
+        console.log('err:', err.messsage);
+    }
+}
+
+
 
 function getUser(id)
 {
