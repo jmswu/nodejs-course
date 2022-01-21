22833ca2a821e5972eeec30a3989614855f4968b
James Wu <james@miracle.co.nz>
2022-01-21 14:03:01 +1300

    added courseSchema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playround')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
